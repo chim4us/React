e9d01228e83ef934972ebc7f009082a18a8d7910
Frank Chimdi <33489950+chim4us@users.noreply.github.com>
2021-04-29 23:51:13 +0100

    Add files via upload
    first commit

---
+++ components/Coin/Coin.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const TDR = styled.td`
+    border: 1px solid #cccccc;
+    width: 25vh;
+`;
+
+export default class Coin extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            price : this.props.price 
+        }
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    /*
+    componentDidMount(){
+        const callback = () => {
+            const ramdomPercentage = 0.995 + Math.random() * 0.01;
+
+            this.setState(
+                function(oldState){
+                    return{
+                        price: oldState.price * ramdomPercentage
+                    };
+                }
+            );
+        }
+
+        
+
+        setInterval(callback, 1000);
+    }
+
+    */
+
+    handleClick(event){
+        event.preventDefault();
+
+        this.props.handleRefresh(this.props.ticker);
+        /*
+        const ramdomPercentage = 0.995 + Math.random() * 0.01;
+
+            this.setState(
+                function(oldState){
+                    return{
+                        price: oldState.price * ramdomPercentage
+                    };
+                }
+            );*/
+    }
+
+    render() {
+        return(
+            <tr className="coin-row">
+                <TDR>{this.props.name}</TDR>
+                <TDR>{this.props.ticker}</TDR>
+                <TDR>${this.state.price}</TDR>
+                <TDR>
+                    <form>
+                        <button onClick={this.handleClick}>Refresh</button>
+                    </form>
+                </TDR>
+            </tr>
+        );
+    }
+}
+
+Coin.propTypes = {
+    name: PropTypes.string.isRequired,
+    ticker: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired
+}
